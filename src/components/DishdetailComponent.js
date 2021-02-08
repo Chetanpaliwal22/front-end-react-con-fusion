a5a2593c13f81a52620b9664864124956c77c1b1
Chetanpaliwal22 <chetanpaliwal22@gmail.com>
2021-02-08 07:35:40 -0500

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -24,7 +24,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments != null) {
         const formattedComment = comments.map(comment => {
 
@@ -38,7 +38,7 @@ function RenderComments({ comments }) {
         return (
             <ul className="list-unstyled">
                 {formattedComment}
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </ul>
         )
     }
@@ -67,7 +67,9 @@ const DishDetail = props => {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments}
+                            addComment={props.addComment}
+                            dishId={props.dish.id} />
                     </div>
                 </div>
             </div>
@@ -105,8 +107,9 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        console.log('Current state is: ' + JSON.stringify(values));
-        alert('Current state is: ' + JSON.stringify(values));
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        // console.log('Current state is: ' + JSON.stringify(values));
+        // alert('Current state is: ' + JSON.stringify(values));
     }
 
     render() {
