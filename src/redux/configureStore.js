f7511735cd461f1fe1009f49ddba3107848b1055
Chetanpaliwal22 <chetanpaliwal22@gmail.com>
2021-02-11 07:30:37 -0500

    React redux form 2

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Comments} from './comments';
+import {Dishes} from './dishes';
+import {Promotions} from './promotions';
+import {Leaders} from './leaders';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger'; 
+import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -12,7 +14,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             leaders: Leaders,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
