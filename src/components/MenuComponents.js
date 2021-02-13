e4d085bc651c1d725c50e8492101921bc79ec6b0
Chetanpaliwal22 <chetanpaliwal22@gmail.com>
2021-02-13 12:45:12 -0500

    fetch

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {
-    Card, CardImg, CardImgOverlay,
-    CardTitle, Breadcrumb, BreadcrumbItem
-} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderMenuItem({ dish, onClick }) {
     return (
         <Card>
             <Link to={`/menu/${dish.id}`} >
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
@@ -19,14 +17,13 @@ function RenderMenuItem({ dish, onClick }) {
     );
 }
 
-const Menu = (props) => {
-
-    const menu = props.dishes.dishes.map((dish) => {
+const Menu = props => {
+    const menu = props.dishes.dishes.map(dish => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
-        );
+        )
     });
 
     if (props.dishes.isLoading) {
@@ -66,7 +63,5 @@ const Menu = (props) => {
             </div>
         );
 }
-
-
 
 export default Menu;
